af4af782cebb3eab4251d8e9a2a22cebaa5d79a1
Shambu Prasad jakka <115019793+SHAMBUPRASAD26@users.noreply.github.com>
2024-03-17 11:40:51 +0530

    Delete remainabalance.py

--- remainabalance.py
+++
@@ -1,8 +0,0 @@
-class balance:
-    def __init__(self):
-        self.a=3000000
-    def display(self,x,y):
-        self.a=self.a-x
-        self.a=self.a+y
-        print("The remaining balance is ",self.a)
-
