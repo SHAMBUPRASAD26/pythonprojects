5d081324995a807f5a18ac0c8f30514e403cad68
Shambu Prasad jakka <115019793+SHAMBUPRASAD26@users.noreply.github.com>
2024-03-17 11:39:04 +0530

    Delete import csv.py

--- import csv.py
+++
@@ -1,8 +0,0 @@
-import csv
-f=open("jsp.csv","a",newline="")
-a=csv.writer(f)
-a.writerow(["studentid","roll","phn"])
-x=int(input("enter id:"))
-y=int(input("enter roll:"))
-z=int(input("enter phn:"))
-a.writerow([x,y,z])
